e49bc92d88583351816db92db54d1553e8867bb7
Sachin <sgmudigoudra@gmail.com>
2024-11-24 10:23:52 +0530

    defect fixed 3457 fixed null values not to be stored in database

---
+++ src/main/java/com/post/A.java
@@ -0,0 +1,9 @@
+package com.post;
+
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println("hello Sachin");
+    }
+
+}
---
+++ src/main/java/com/post/B.java
@@ -0,0 +1,7 @@
+package com.post;
+
+public class B {
+    public void test(){
+        System.out.println(100);
+    }
+}
